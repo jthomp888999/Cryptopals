ea95ef35f093b0598dea138003db528130b81139
jthomp888999 <jthomp@protonmail.com>
2016-10-04 16:07:03 -0500

    just getting caught up

--- Challenge 1/challenge1.py
+++ Challenge 1/challenge1.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-import sys, binascii
+import binascii
 
 string = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
 answer = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
@@ -8,6 +8,7 @@ def converter():
     hex = string.decode("hex")
     b64 = binascii.b2a_base64(hex)
     return b64
+
 result = converter()
 result = result.rstrip('\n')
 if result == answer:
--- Challenge 2/challenge2.py
+++ Challenge 2/challenge2.py
@@ -1,14 +1,16 @@
-import sys, binascii
+import binascii
 from Crypto.Util.strxor import strxor
 
 
-string = sys.argv[1]
+string = "1c0111001f010100061a024b53535009181c"
 key = "686974207468652062756c6c277320657965"
 
-def fixed_XOR():
+# Simply XOR's provided string with provided key
+def fixed_XOR(s,k):
     s = binascii.unhexlify(string)
     k = binascii.unhexlify(key)
     answer = strxor(s, k)
-    print("\n\n")
     return answer
 
+result = fixed_XOR(string, key)
+print result
