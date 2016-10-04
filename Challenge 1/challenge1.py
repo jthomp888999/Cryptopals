d9390b6edfcdf01f00bef04eaf291fe8d5ecf3de
jthomp888999 <jthomp@protonmail.com>
2016-10-04 14:05:10 -0500

    fix

--- Challenge 1/challenge1.py
+++ Challenge 1/challenge1.py
@@ -2,9 +2,15 @@
 
 import sys, binascii
 
-string = sys.argv[1]
+string = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+answer = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
 def converter():
     hex = string.decode("hex")
     b64 = binascii.b2a_base64(hex)
-    print("\n\n")
     return b64
+result = converter()
+result = result.rstrip('\n')
+if result == answer:
+    print 'yes'
+else:
+    print 'no'
